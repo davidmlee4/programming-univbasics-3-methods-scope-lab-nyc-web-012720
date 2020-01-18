701dadb2d81f700df20cf63831738d6f2ba482a5
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-18 03:53:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,7 +1,9 @@
 def mario
-  phrase = 'It's-a me, Mario!'
+  phrase = "It's-a me, Mario!"
   puts "#{phrase}"
 end
+
+mario
 
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
